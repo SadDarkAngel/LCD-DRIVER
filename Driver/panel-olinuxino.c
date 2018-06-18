1af4da78e10eb3ce4af741fe7a450957353e22e9
Stefan Mavrodiev <stefan.mavrodiev@gmail.com>
2018-06-18 14:45:21 +0300

    Update driver documentation

--- Driver/panel-olinuxino.c
+++ Driver/panel-olinuxino.c
@@ -26,22 +26,38 @@
 #define LCD_OLINUXINO_HEADER_MAGIC	0x4F4CB727
 #define LCD_OLINUXINO_DATA_LEN		256
 
+struct lcd_olinuxino_mode {
+	u32 pixelclock;
+	u32 hactive;
+	u32 hfp;
+	u32 hbp;
+	u32 hpw;
+	u32 vactive;
+	u32 vfp;
+	u32 vbp;
+	u32 vpw;
+	u32 refresh;
+	u32 flags;
+};
+
+struct lcd_olinuxino_info {
+	char name[32];
+	u32 width_mm;
+	u32 height_mm;
+	u32 bpc;
+	u32 bus_format;
+	u32 bus_flag;
+} __attribute__((__packed__));
+
 struct lcd_olinuxino_eeprom {
-	u32 header;				// 4
-	u32 id;					// 8
-	u32 revision;				// 12
-	u32 serial;				// 16
-	struct display_timing timing;		// 128
-	struct {
-		char name[32];			// 160
-		u32 width_mm;			// 164
-		u32 height_mm;			// 168
-		u32 bpc;			// 172
-		u32 bus_format;			// 176
-		u32 bus_flag;			// 180
-	} info;
-	u8 unused[72];				// 252
-	u32 checksum;				// 256
+	u32 header;
+	u32 id;
+	char revision[4];
+	u32 serial;
+	struct lcd_olinuxino_info info;
+	u32 num_modes;
+	u8 reserved[180];
+	u32 checksum;
 } __attribute__((__packed__));
 
 struct lcd_olinuxino {
@@ -138,46 +154,61 @@ static int lcd_olinuxino_get_modes(struct drm_panel *panel)
 {
 	struct lcd_olinuxino *lcd = to_lcd_olinuxino(panel);
 	struct drm_connector *connector = lcd->panel.connector;
-	struct display_timing *timing = &lcd->eeprom.timing;
+	struct lcd_olinuxino_info *lcd_info = &lcd->eeprom.info;
 	struct drm_device *drm = lcd->panel.drm;
+	struct lcd_olinuxino_mode *lcd_mode;
 	struct drm_display_mode *mode;
-	struct videomode vm;
+	int i, num = 0;
 
-	videomode_from_timing(timing, &vm);
-	mode = drm_mode_create(drm);
-	if (!mode) {
-		dev_err(drm->dev, "failed to add mode %ux%u\n",
-			timing->hactive.typ, timing->vactive.typ);
-			return 0;
+	for (i = 0; i < lcd->eeprom.num_modes; i++) {
+		lcd_mode = (struct lcd_olinuxino_mode *)
+			   &lcd->eeprom.reserved[i * sizeof(*lcd_mode)];
+
+		mode = drm_mode_create(drm);
+		if (!mode) {
+			dev_err(drm->dev, "failed to add mode %ux%u@%u\n",
+				lcd_mode->hactive,
+				lcd_mode->vactive,
+				lcd_mode->refresh);
+				continue;
+		}
+
+		mode->clock = lcd_mode->pixelclock;
+		mode->hdisplay = lcd_mode->hactive;
+		mode->hsync_start = lcd_mode->hactive + lcd_mode->hfp;
+		mode->hsync_end = lcd_mode->hactive + lcd_mode->hfp +
+				  lcd_mode->hpw;
+		mode->htotal = lcd_mode->hactive + lcd_mode->hfp +
+			       lcd_mode->hpw + lcd_mode->hbp;
+		mode->vdisplay = lcd_mode->vactive;
+		mode->vsync_start = lcd_mode->vactive + lcd_mode->vfp;
+		mode->vsync_end = lcd_mode->vactive + lcd_mode->vfp +
+				  lcd_mode->vpw;
+		mode->vtotal = lcd_mode->vactive + lcd_mode->vfp +
+			       lcd_mode->vpw + lcd_mode->vbp;
+		mode->vrefresh = lcd_mode->refresh;
+
+		if (lcd->eeprom.num_modes == 1)
+			mode->type |= DRM_MODE_TYPE_PREFERRED;
+		mode->type |= DRM_MODE_TYPE_DRIVER;
+
+		drm_mode_set_name(mode);
+		drm_mode_probed_add(connector, mode);
+
+		num++;
 	}
 
-	drm_display_mode_from_videomode(&vm, mode);
+	memcpy(connector->display_info.name, lcd_info->name, 32);
+	connector->display_info.width_mm = lcd_info->width_mm;
+	connector->display_info.height_mm = lcd_info->height_mm;
+	connector->display_info.bpc = lcd_info->bpc;
 
-	mode->type |= DRM_MODE_TYPE_DRIVER | DRM_MODE_TYPE_PREFERRED;
+	if (lcd_info->bus_format)
+		drm_display_info_set_bus_formats(&connector->display_info,
+						 &lcd_info->bus_format, 1);
+	connector->display_info.bus_flags = lcd_info->bus_flag;
 
-	memcpy(connector->display_info.name, lcd->eeprom.info.name, 32);
-	connector->display_info.width_mm = lcd->eeprom.info.width_mm;
-	connector->display_info.height_mm = lcd->eeprom.info.height_mm;
-	connector->display_info.bpc = lcd->eeprom.info.bpc;
-
-	drm_display_info_set_bus_formats(&connector->display_info,
-					 &lcd->eeprom.info.bus_format, 1);
-	connector->display_info.bus_flags = lcd->eeprom.info.bus_flag;
-
-	drm_mode_probed_add(connector, mode);
-	return 1;
-}
-
-static int lcd_olinuxino_get_timings(struct drm_panel *panel,
-				     unsigned int num_timings,
-				     struct display_timing *timings)
-{
-	struct lcd_olinuxino *lcd = to_lcd_olinuxino(panel);
-
-	if (timings)
-		timings = &lcd->eeprom.timing;
-
-	return 1;
+	return num;
 }
 
 struct drm_panel_funcs lcd_olinuxino_funcs = {
@@ -186,7 +217,6 @@ struct drm_panel_funcs lcd_olinuxino_funcs = {
 	.prepare = lcd_olinuxino_prepare,
 	.enable = lcd_olinuxino_enable,
 	.get_modes = lcd_olinuxino_get_modes,
-	.get_timings = lcd_olinuxino_get_timings,
 };
 
 static int lcd_olinuxino_probe(struct i2c_client *client,
@@ -195,11 +225,8 @@ static int lcd_olinuxino_probe(struct i2c_client *client,
 	struct device *dev = &client->dev;
 	struct device_node *backlight;
 	struct lcd_olinuxino *lcd;
-
-	int ret = 0;
-	int i;
-
 	u32 checksum;
+	int i, ret = 0;
 
 	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C ||
 				     I2C_FUNC_SMBUS_READ_I2C_BLOCK))
@@ -224,7 +251,7 @@ static int lcd_olinuxino_probe(struct i2c_client *client,
 						    (u8 *)&lcd->eeprom + i);
 		mutex_unlock(&lcd->mutex);
 		if (ret < 0) {
-			dev_err(dev, "Error reading from device at %02x\n", i);
+			dev_err(dev, "error reading from device at %02x\n", i);
 			return ret;
 		}
 	}
@@ -232,17 +259,20 @@ static int lcd_olinuxino_probe(struct i2c_client *client,
 	/* Check configuration checksum */
 	checksum = ~crc32(~0, (u8 *)&lcd->eeprom, 252);
 	if (checksum != lcd->eeprom.checksum) {
-		dev_err(dev, "Configuration checksum does not match!\n");
-		dev_dbg(dev, "Calculated checksum: %08x\n", checksum);
-		dev_dbg(dev, "Stored checksum: %08x\n", lcd->eeprom.checksum);
+		dev_err(dev, "configuration checksum does not match!\n");
 		return -EINVAL;
 	}
 
 	/* Check magic header */
 	if (lcd->eeprom.header != LCD_OLINUXINO_HEADER_MAGIC) {
-		dev_err(dev, "Magic header does not match\n");
+		dev_err(dev, "magic header does not match\n");
 		return -EINVAL;
 	}
+
+	dev_info(dev, "Detected %s, Rev. %s, Serial: %08x\n",
+		 lcd->eeprom.info.name,
+		 lcd->eeprom.revision,
+		 lcd->eeprom.serial);
 
 	lcd->enabled = false;
 	lcd->prepared = false;
